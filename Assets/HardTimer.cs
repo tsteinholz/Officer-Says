3bfad83cd4415ac66b16d3e0b6eb64e9241ef0d6
Thoams Steinholz <tsteinholz@yahoo.com>
2016-10-02 02:59:43 -0400

    timers

---
+++ Assets/HardTimer.cs
@@ -0,0 +1,27 @@
+﻿/*using UnityEngine;
+using System.Collections;
+
+float timeRemaining=5;
+public class HardTimer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeRemaining -= Time.deltaTime;
+	}
+
+
+	void OnGUI(){
+
+		if (timeRemaining > 0) {
+			GUI.Label (new Rect (100, 100, 200, 100), "Time Remaining: " + (int)timeRemaining);
+		} else {
+			GUI.Label(new Rect(100, 100, 100, 100), "Please proceed out of your vehicle.");
+
+		}
+}
+*/
