dbf5828f76e79cc1220f5e8888859e8657c33637
Thoams Steinholz <tsteinholz@yahoo.com>
2016-10-02 07:49:55 -0400

    woohoo

---
+++ Assets/CameraLock.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLock : MonoBehaviour {
+
+	public float min;
+	public float max;
+
+	Transform trans;
+
+	void Start () {
+		trans = GetComponent<Transform> ();
+	}
+
+	void LateUpdate () {
+		//if (trans.transform.position.y < min) {
+		//	trans.transform.position.y = min;
+		//} else if (trans.transform.position.y > max) {
+		//	trans.transform.position.y = max;
+		//}
+	}
+}
