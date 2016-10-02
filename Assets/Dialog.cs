d9ab60dc7147e2573c74fac29438ae84c630f1b1
Thomas Steinholz <tsteinholz@yahoo.com>
2016-10-02 04:22:22 -0400

    work on logic

---
+++ Assets/Dialog.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dialog : MonoBehaviour {
+
+	Meta curr_meta;
+
+	// Use this for initialization
+	void Start () {
+		curr_meta = GetComponent<Meta>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void GetDialog(string key) {
+		
+	}
+}
---
+++ Assets/Meta.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum difficulty
+{
+	DIFFICULTY_EASY,
+	DIFFICULTY_HARD,
+}
+
+public class Meta : MonoBehaviour {
+
+	[Range(0,1)]
+	public int difficulty;
+
+	void Start () {
+		difficulty = Random.Range (0, 1);
+	}
+}
